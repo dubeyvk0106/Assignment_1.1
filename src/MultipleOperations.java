9f62c8dcf7723a3ddf4510f1b522072dcc4c4368
Vivek Dubey <dubeyvk0106@gmail.com>
2023-02-28 19:28:50 +0530

    Initial Commit

--- src/MultipleOperations.java
+++ src/MultipleOperations.java
@@ -1,15 +1,15 @@
 public class MultipleOperations {
     public static void main(String[] args){
 
-        double a = 5;
-        double b = 2.5;
+        double t = 5;
+        double u = 2.5;
 
-        double c = a + b;
-        double d = a - b;
-        double e = a*b;
-        double f = a/b;
-        double g = a%b;
+        double v = t + u;
+        double w = t - u;
+        double x = t*u;
+        double y = t/u;
+        double z = t%u;
 
-        System.out.println("Sum = "+ c + " subtraction ="+d + " multiply=" +e + "division = "+f + " and rem ="+g);
+        System.out.println("Sum = "+ v + " subtraction ="+w + " multiply=" +x + "division = "+y + " and rem ="+z);
     }
 }
