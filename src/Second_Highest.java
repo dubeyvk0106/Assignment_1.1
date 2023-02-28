fd791ca1480660eddbd9e8c06ab3552ac389d3c7
Vivek Dubey <dubeyvk0106@gmail.com>
2023-02-28 19:46:08 +0530

    Initial Commit

--- src/Second_Highest.java
+++ src/Second_Highest.java
@@ -2,27 +2,40 @@ import java.util.*;
 
 public class Second_Highest {
 
-    static void secondLargest(int arr[],
-                              int size)
+
+
+    static void secondLargest(int arr[], int n)
     {
-        int i, first, second;
-        if (size < 2)
+
+        if (n < 2)
         {
-            System.out.printf(" Invalid Input ");
+            System.out.println(" Second highest number program requires at least two numbers...  ");
             return;
         }
 
         Arrays.sort(arr);
 
-        System.out.println("Second highest element is :" + arr[size-2]);
+        System.out.println("Second highest element is :" + arr[n-2]);
 
     }
 
     public static void main(String[] args){
 
-        int arr[] = {10, 45, 17, 15, 74, 13};
-        int n = arr.length;
-        secondLargest(arr, n);
+        Scanner input = new Scanner(System.in);
+        System.out.println(" Please Enter Number size of  array : ");
+        int Size;
+        Size = input.nextInt();
+        int i;
+
+        int [] arr = new int[Size];
+
+        System.out.print(" Please Enter " + Size + " elements of an Array  : ");
+        for ( i = 0; i < Size; i++)
+        {
+            arr[i] = input.nextInt();
+        }
+
+        secondLargest(arr, Size);
     }
 
 }
