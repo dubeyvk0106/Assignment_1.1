ef055965c80285d5eb83434f84a976640d81bee1
Vivek Dubey <dubeyvk0106@gmail.com>
2023-02-28 19:52:27 +0530

    Initial Commit

--- src/Sum_Even.java
+++ src/Sum_Even.java
@@ -2,30 +2,31 @@ import java.util.*;
 
 public class Sum_Even {
 
-    private static Scanner input;
+
     public static void main(String[] args)
     {
-        int Size, i, sumEven = 0;
-        input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+        int n, j, sumEven = 0;
 
-        System.out.print(" Please Enter Number size of  array : ");
-        Size = input.nextInt();
 
-        int [] arr = new int[Size];
+        System.out.print(" Please Enter size of the array you want sum of : ");
+        n = input.nextInt();
 
-        System.out.print(" Please Enter " + Size + " elements of an Array  : ");
-        for (i = 0; i < Size; i++)
+        int [] arr = new int[n];
+
+        System.out.print(" Please Enter " + n + " elements of an the Array  : ");
+        for (j = 0; j < n; j++)
         {
-            arr[i] = input.nextInt();
+            arr[j] = input.nextInt();
         }
 
-        for(i = 0; i < Size; i++)
+        for(j = 0; j < n; j++)
         {
-            if(arr[i] % 2 == 0)
+            if(arr[j] % 2 == 0)
             {
-                sumEven = sumEven + arr[i];
+                sumEven = sumEven + arr[j];
             }
         }
-        System.out.println("\n The Sum of Even Numbers in this Array = " + sumEven);
+        System.out.println(" The Sum of Even Numbers in the provided array = " + sumEven);
     }
 }
