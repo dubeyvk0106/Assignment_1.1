b7d8137ea325417455933fcaf54a5d9d596f12b4
Vivek Dubey <dubeyvk0106@gmail.com>
2023-03-15 16:55:00 +0530

    Initial

--- src/week6/ClassNotFoundException.java
+++ src/week6/ClassNotFoundException.java
@@ -3,7 +3,7 @@ package week6;
 public class ClassNotFoundException {
     public static void main(String[] args) {
         try{
-            Class c = Class.forName("RandomClass");   // Class1 is not defined
+            Class c = Class.forName("RandomClass");
         }
         catch(Exception e){
             System.out.println(e);
