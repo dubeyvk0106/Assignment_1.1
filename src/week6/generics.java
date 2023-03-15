c6de64236f828bdf78c23e95d0e0495f3d897a4f
Vivek Dubey <dubeyvk0106@gmail.com>
2023-03-15 10:38:34 +0530

    Initial

---
+++ src/week6/generics.java
@@ -0,0 +1,28 @@
+package week6;
+
+class GenericClass<T> {
+
+    private T data;
+
+    public GenericClass(T data) {
+        this.data = data;
+    }
+
+
+    public T getValue() {
+        return this.data;
+    }
+}
+
+public class generics {
+
+    public static void main(String[] args) {
+
+
+        GenericClass<Integer> integerObj = new GenericClass<>(100);
+        System.out.println("Generic Class returns: " + integerObj.getValue());
+
+        GenericClass<String> stringObj = new GenericClass<>("Java Generics Practice");
+        System.out.println("Generic Class returns: " + stringObj.getValue());
+    }
+}
