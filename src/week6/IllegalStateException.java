1e8e5e66d154ee2d425ca0188e7ea9333718115b
Vivek Dubey <dubeyvk0106@gmail.com>
2023-03-15 16:41:13 +0530

    Initial

---
+++ src/week6/IllegalStateException.java
@@ -0,0 +1,25 @@
+package week6;
+import java.io.*;
+import java.util.Scanner;
+
+class addition{
+    public static void print(int a,int b)
+    {
+        System.out.println("Addition of Positive Integers :"+(a+b));
+    }
+}
+
+public class IllegalStateException {
+
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        if (n1 >= 0 && n2 >= 0) {
+            addition.print(n1, n2);
+        } else {
+            throw new Exception("Only positive values are allowed ");
+        }
+    }
+}
