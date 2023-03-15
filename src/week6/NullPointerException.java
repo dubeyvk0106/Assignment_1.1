d7fa19fc699c315ff4c0b136883ac0b6d3397aa6
Vivek Dubey <dubeyvk0106@gmail.com>
2023-03-15 15:22:22 +0530

    Initial

--- src/week6/NullPointerException.java
+++ src/week6/NullPointerException.java
@@ -5,7 +5,7 @@ public class NullPointerException extends Throwable {
     public static void main(String args[])
     {
         try {
-            String b = null; //null value
+            String b = null;
             System.out.println(b.charAt(0));
         } catch(Exception e) {
             System.out.println("Exception occurred is as follows : " + e.getMessage());
