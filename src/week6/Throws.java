084b81114626ce450156b1ca59aa26f9052c88cb
Vivek Dubey <dubeyvk0106@gmail.com>
2023-03-15 12:01:20 +0530

    Initial

---
+++ src/week6/Throws.java
@@ -0,0 +1,24 @@
+package week6;
+
+public class Throws {
+
+    static void function() throws IllegalAccessException
+    {
+        System.out.println("Inside function(). ");
+        System.out.println("Checking for throws ");
+        throw new IllegalAccessException("e");
+    }
+    public static void main(String args[])
+    {
+        try
+        {
+            function();
+        }
+        catch(IllegalAccessException e)
+        {
+            System.out.println("It's been Caught in main function .");
+            System.out.println("Successful catch operation");
+            System.out.println("Thanks !!");
+        }
+    }
+}
